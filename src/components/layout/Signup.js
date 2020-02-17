4487d9ae06a401dc4429d134a685be03078afc90
zubayerhimel <zhimel007@gmail.com>
2020-02-17 23:08:44 +0600

    fixed some error from signup page

--- src/components/layout/Signup.js
+++ src/components/layout/Signup.js
@@ -8,63 +8,71 @@ class Signin extends Component {
     return (
       <div className="contents">
         <form action="">
-          <div class="container">
-            <div class="row">
-              <div class="center-align">
-                <h4 class="orange-text text-darken-3">Sign Up</h4>
+          <div className="container">
+            <div className="row">
+              <div className="center-align">
+                <h4 className="orange-text text-darken-3">Sign Up</h4>
               </div>
-              <div class="card col s12 l12 z-depth-2 card-border">
-                <div class="card-content">
-                  <div class="col s6 l6">
+              <div className="card col s12 l12 z-depth-2 card-border">
+                <div className="card-content">
+                  <div className="col s6 l6">
                     <img
                       id="info-svg-signup"
                       width="500"
-                      class="responsive-img"
+                      className="responsive-img"
                       src={signupsvg}
                     />
                     <br />
                     <br />
                   </div>
-                  <div class="col s12 l6" id="info-input-field">
-                    <div class="row">
-                      <div class="input-field col s12 l12">
-                        <input id="last_name" type="text" class="validate" />
-                        <label for="last_name">ID</label>
+                  <div className="col s12 l6" id="info-input-field">
+                    <div className="row">
+                      <div className="input-field col s12 l12">
+                        <input
+                          id="last_name"
+                          type="text"
+                          className="validate"
+                        />
+                        <label htmlFor="last_name">ID</label>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="input-field col s12 l12">
-                        <input id="email" type="email" class="validate" />
-                        <label for="email">Email</label>
+                    <div className="row">
+                      <div className="input-field col s12 l12">
+                        <input id="email" type="email" className="validate" />
+                        <label htmlFor="email">Email</label>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="input-field col s12 l12">
-                        <input id="password" type="password" class="validate" />
-                        <label for="password">Password</label>
+                    <div className="row">
+                      <div className="input-field col s12 l12">
+                        <input
+                          id="password"
+                          type="password"
+                          className="validate"
+                        />
+                        <label htmlFor="password">Password</label>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="input-field col s12 l12">
+                    <div className="row">
+                      <div className="input-field col s12 l12">
                         <input
                           id="cpassword"
                           type="password"
-                          class="validate"
+                          className="validate"
                         />
-                        <label for="cpassword">Confirm Password</label>
+                        <label htmlFor="cpassword">Confirm Password</label>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="center-align">
+                    <div className="row">
+                      <div className="center-align">
                         <a
                           href="signin.html"
-                          class="waves-effect btn bgc borderRadius"
+                          className="waves-effect btn bgc borderRadius"
                         >
                           Register
                         </a>
                         <br />
                         <br />
-                        <span class="clink">
+                        <span className="clink">
                           Already have an account?&nbsp;{" "}
                           <NavLink exact to="/signin">
                             SignIn
