7570375dd25c974ae2b7c533a83ff55697098b85
zubayerhimel <zhimel007@gmail.com>
2020-02-18 02:22:00 +0600

    componentDidMount for time remaining

--- src/components/layout/Subscription.js
+++ src/components/layout/Subscription.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/style.css";
 
-export default function Subscription() {
-  return (
-    <div>
-      <h1>Subscription</h1>
-    </div>
-  );
+class Subscription extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 100 };
+  }
+  render() {
+    return (
+      <div className="contents container">
+        <h4 className="fgc">Subscription</h4>
+        <div className="row">
+          <div className="col s12 l8 offset-l2">
+            <div className="card">
+              <div className="card-content">
+                <h3>Time Remains</h3>
+                <h2> {this.state.count} </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    this.myInterval = setInterval(() => {
+      this.setState(prevState => ({
+        count: prevState.count - 1
+      }));
+    }, 1000);
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.myInterval);
+  };
 }
+
+export default Subscription;
